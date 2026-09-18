54f840e8220682372dcc9b496f6f895e2b88c066
YokoRyoKiz <ryouyayokoi@gmail.com>
2026-09-18 12:22:20 +0900

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/notion-api': {
+        target: 'https://api.notion.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/notion-api/, ''),
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader('Authorization', 'Bearer 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');
+            proxyReq.setHeader('Notion-Version', '2022-06-28');
+          });
+        }
+      }
+    }
+  }
+})

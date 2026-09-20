8feb05b5bc17107d4d700d6e86c1c228d4214739
YokoRyoKiz <ryouyayokoi@gmail.com>
2026-09-20 13:40:00 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,26 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/notion-api': {
-        target: 'https://api.notion.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/notion-api/, ''),
-        configure: (proxy, options) => {
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            proxyReq.setHeader('Authorization', 'Bearer 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');
-            proxyReq.setHeader('Notion-Version', '2022-06-28');
-          });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: './',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/notion-api': {
+          target: 'https://api.notion.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/notion-api/, ''),
+          configure: (proxy, options) => {
+            proxy.on('proxyReq', (proxyReq, req, res) => {
+              proxyReq.setHeader('Authorization', `Bearer ${env.NOTION_API_KEY}`);
+              proxyReq.setHeader('Notion-Version', '2022-06-28');
+            });
+          }
         }
       }
     }
-  }
+  };
 })
